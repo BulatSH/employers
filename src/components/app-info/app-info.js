16b6e9fa49f7ba4aaa64e4a7441fd4088066cc87
Bulat Sharafutdinov <hufdgdgdgfd@mail.ru>
2023-02-12 15:05:39 +0300

    fix: создать компоненты

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,17 @@
+const AppInfo = () => {
+	return (
+		<div className="app-info">
+			<h1>
+				Учет сотрудников в компании
+			</h1>
+			<h2>
+				Общее число сотрудников:
+			</h2>
+			<h2>
+				Премию получат:
+			</h2>
+		</div>
+	);
+}
+
+export default AppInfo;
